d22170f9aced00c48ce71c18f86374c376b6001a
Vania Dimitrova <vani4ka66@abv.bg>
2017-09-20 11:09:28 +0300

    Create 07. Filtered-Ads-with-their-Category, Town-and-Status.sql

---
+++ 01. Database-Sampe-Exam/07. Filtered-Ads-with-their-Category, Town-and-Status.sql
@@ -0,0 +1,10 @@
+select a.Title, c.Name as CategoryName, t.Name as TownName, ad.[Status]
+from Ads a
+left join Towns t
+on t.Id = a.TownId
+left join Categories c
+on c.Id = a.CategoryId
+left join AdStatuses ad
+on ad.Id = a.StatusId
+where t.Name in ('Sofia', 'Blagoevgrad', 'Stara Zagora') and Status = 'Published'
+order by a.Title
